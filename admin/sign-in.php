a35f3de013bc5a00d8f351111ba9db2c6ebf5ee3
jielyh <hyper_lil_angel_905@hotmail.com>
2012-04-25 22:21:42 -0400

    Minor css

--- admin/sign-in.php
+++ admin/sign-in.php
@@ -64,7 +64,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     
         </form>
         
-        <div class="login">
+        <div class="login buttons-css">
         	<a href="../index.php">Home</a>
     	</div>
      </div>
